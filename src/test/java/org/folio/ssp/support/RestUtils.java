9de60fc9503854d3b93dd49415f62fa55272dd3e
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-09-19 21:33:25 +0300

    feat(testing): add tests for unauthorized and forbidden user clients

--- src/test/java/org/folio/ssp/support/RestUtils.java
+++ src/test/java/org/folio/ssp/support/RestUtils.java
@@ -14,9 +14,23 @@ public class RestUtils {
 
   private static final RestAssuredConfig USER_CLIENT_CONFIG = userClientConfig();
   private static final RestAssuredConfig ADMIN_CLIENT_CONFIG = adminClientConfig();
+  private static final RestAssuredConfig UNAUTH_USER_CLIENT_CONFIG = unauthUserClientConfig();
+  private static final RestAssuredConfig FORBIDDEN_USER_CLIENT_CONFIG = forbiddenUserClientConfig();
+
+  private static final String KEYSTORE_PASS = "supersecret";
+  private static final String FSSP_CLIENT_TRUSTSTORE = "certificates/client/fssp-client-truststore.p12";
+  private static final String PKCS_12 = "pkcs12";
 
   public static RequestSpecification givenUserClient() {
     return RestAssured.given().config(USER_CLIENT_CONFIG);
+  }
+
+  public static RequestSpecification givenUnauthorizedUserClient() {
+    return RestAssured.given().config(UNAUTH_USER_CLIENT_CONFIG);
+  }
+
+  public static RequestSpecification givenForbiddenUserClient() {
+    return RestAssured.given().config(FORBIDDEN_USER_CLIENT_CONFIG);
   }
 
   public static RequestSpecification givenAdminClient() {
@@ -25,20 +39,40 @@ public class RestUtils {
 
   private static RestAssuredConfig userClientConfig() {
     return RestAssured.config().sslConfig(new SSLConfig()
-      .keyStore("certificates/client/fssp-user-client-keystore.p12", "supersecret")
-      .keystoreType("pkcs12")
-      .trustStore("certificates/client/fssp-client-truststore.p12", "supersecret")
-      .trustStoreType("pkcs12")
+      .keyStore("certificates/client/fssp-user-client-keystore.p12", KEYSTORE_PASS)
+      .keystoreType(PKCS_12)
+      .trustStore(FSSP_CLIENT_TRUSTSTORE, KEYSTORE_PASS)
+      .trustStoreType(PKCS_12)
       .allowAllHostnames()
     );
   }
 
   private static RestAssuredConfig adminClientConfig() {
     return RestAssured.config().sslConfig(new SSLConfig()
-      .keyStore("certificates/client/fssp-admin-client-keystore.p12", "supersecret")
-      .keystoreType("pkcs12")
-      .trustStore("certificates/client/fssp-client-truststore.p12", "supersecret")
-      .trustStoreType("pkcs12")
+      .keyStore("certificates/client/fssp-admin-client-keystore.p12", KEYSTORE_PASS)
+      .keystoreType(PKCS_12)
+      .trustStore(FSSP_CLIENT_TRUSTSTORE, KEYSTORE_PASS)
+      .trustStoreType(PKCS_12)
+      .allowAllHostnames()
+    );
+  }
+
+  private static RestAssuredConfig unauthUserClientConfig() {
+    return RestAssured.config().sslConfig(new SSLConfig()
+      .keyStore("certificates/client/fssp-unauth-user-client-keystore.p12", KEYSTORE_PASS)
+      .keystoreType(PKCS_12)
+      .trustStore(FSSP_CLIENT_TRUSTSTORE, KEYSTORE_PASS)
+      .trustStoreType(PKCS_12)
+      .allowAllHostnames()
+    );
+  }
+
+  private static RestAssuredConfig forbiddenUserClientConfig() {
+    return RestAssured.config().sslConfig(new SSLConfig()
+      .keyStore("certificates/client/fssp-forbidden-user-client-keystore.p12", KEYSTORE_PASS)
+      .keystoreType(PKCS_12)
+      .trustStore(FSSP_CLIENT_TRUSTSTORE, KEYSTORE_PASS)
+      .trustStoreType(PKCS_12)
       .allowAllHostnames()
     );
   }
